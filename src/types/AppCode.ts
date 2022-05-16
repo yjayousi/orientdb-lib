217fd76e8324f4fc6a45a12a2553a9c31deac4e8
Nitin <nitin@blipiq.com>
2022-05-16 14:21:20 +0530

    minor change

--- src/types/AppCode.ts
+++ src/types/AppCode.ts
@@ -8,7 +8,7 @@ export function getAppCodeMessage(appCode: AppCode, arg1?): string {
         case AppCode.VertexNotFound:
             return `Vertex ${arg1} not found`;
         case AppCode.VertexAlreadyExists:
-            return `A vertex ${arg1} already exists`
+            return `Vertex ${arg1} already exists`
         default:
             neverReached(appCode);
     }
